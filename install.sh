c4d19bb7b3248856cb30d377cd059c2e9a4ba14e
secureinfo42 <secureinfo.eu@gmail.com>
2022-07-19 05:20:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 banner() { 
 	printf "\n\n\033[1;34m=== %s \033[0m\n" "$1"
@@ -7,12 +7,13 @@ banner() {
 banner "Downloading"
 curl 'https://github.com/secureinfo42/dotfiles/raw/main/dot_files.tbz' -O dot_files.tbz
 
-banner "Creating backup"
+banner "Creating backup : dot_files-backup-$date.tbz"
 date=`date +"%Y-%m-%d@%H-%M"`
-tar cjfv dot_files-backup-$date.tbz ~/.ssh/config ~/.config/htop/htoprc ~/.vim* ~/.zsh*
+cd ~
+tar cjf dot_files-backup-$date.tbz ~/.ssh/config ~/.config/htop/htoprc ~/.vim* ~/.zsh*
 
 banner "Extracting downloaded files"
-tar xjfv dot_files.tbz -C ~/
+tar xjf dot_files.tbz -C ~/
 
 
 
